307e39403bd3d2ffcb0a6429bdffaa0ce84a14ae
DanielK <saponembigas@gmail.com>
2025-11-28 19:58:44 +0700

    tzsettingsnotification(feat/et-5): fix

--- src/main/java/dev/kuklin/kworkcalendar/services/scheduler/UserAuthNotificationSchedulerProcessor.java
+++ src/main/java/dev/kuklin/kworkcalendar/services/scheduler/UserAuthNotificationSchedulerProcessor.java
@@ -2,12 +2,15 @@ package dev.kuklin.kworkcalendar.services.scheduler;
 
 import dev.kuklin.kworkcalendar.entities.UserAuthNotification;
 import dev.kuklin.kworkcalendar.library.ScheduleProcessor;
+import dev.kuklin.kworkcalendar.library.tgutils.Command;
+import dev.kuklin.kworkcalendar.library.tgutils.TelegramKeyboard;
 import dev.kuklin.kworkcalendar.services.UserAuthNotificationService;
 import dev.kuklin.kworkcalendar.telegram.AssistantTelegramBot;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 
 import java.util.List;
 
@@ -24,7 +27,8 @@ public class UserAuthNotificationSchedulerProcessor implements ScheduleProcessor
 
         for (UserAuthNotification notification: list) {
             Message returnedMessage = assistantTelegramBot.sendReturnedMessage(
-                    notification.getTelegramId(), notification.getText()
+                    notification.getTelegramId(), notification.getText(),
+                    getAuthButton(), null
             );
 
             if (returnedMessage != null) {
@@ -34,6 +38,13 @@ public class UserAuthNotificationSchedulerProcessor implements ScheduleProcessor
                 log.error("User auth notification error!");
             }
         }
+    }
+
+    public InlineKeyboardMarkup getAuthButton() {
+        TelegramKeyboard.TelegramKeyboardBuilder builder = TelegramKeyboard.builder();
+        builder.row(TelegramKeyboard.button("Авторизоваться", Command.ASSISTANT_AUTH.getCommandText()));
+
+        return builder.build();
     }
 
 
